5da9c934b30dcb1dac8860421ed495622dad9e45
Caio Vinicius <140816471+Isuneblack29@users.noreply.github.com>
2025-04-18 13:52:24 -0300

    Add files via upload

---
+++ impar.py
@@ -0,0 +1,10 @@
+print('VERIFICADOR DE PAR OU IMPAR.by_isune')
+
+numero = int(input('DIGITE O NUMERO: '))
+
+
+if numero % 2 == 0:
+    print('SEU NUMERO É PAR')
+
+else:
+    print('SEU NUMERO É IMPAR')
